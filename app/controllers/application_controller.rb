26a02f7e169b2232614f5d8e93763cf135d5cad2
Nathalie Santana <nathalie.scruz@gmail.com>
2018-12-01 05:48:37 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,25 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+<<<<<<< HEAD
 end
+=======
+
+  patch '/posts/:id' do  #updates a post
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+
+  delete '/posts/:id/delete' do #delete action
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    redirect to '/posts'
+  end
+
+
+end
+>>>>>>> 9c6464d9607186bb92210f4fcc4f928725d5ad16
